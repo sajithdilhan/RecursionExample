e6daad35cd5cbc56a8d54ebab0c5d0456b678c5a
Sajith Dilhan <sajithdilhan@gmail.com>
2022-01-16 22:56:29 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace FactorialExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            int number = fact(input);
+            Console.WriteLine("Factorial of " + input + ": "+number);
+        }
+
+        static int fact(int number)
+        {
+            if (number >= 1)
+            {
+                return number * fact(number - 1);
+            }
+            else
+            {
+                return 1;
+            }
+        }
+    }
+}
